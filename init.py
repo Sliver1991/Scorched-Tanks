56880428f4bdfecb63030990205d582967d1e84e
Slava <slavafid@gmail.com>
2017-10-25 09:41:10 +0300

    init.py created

---
+++ init.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+width, height = pygame.display.list_modes()[0]
+print(width,height)
+gameDisplay = pygame.display.set_mode((width,height))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+pygame.display.toggle_fullscreen()
+
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                  pygame.quit()
+                  quit()  
+    clock.tick(60)
+
