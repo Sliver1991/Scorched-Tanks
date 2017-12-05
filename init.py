5adb82eff7f455b057f0c8c8142f9ceda1e82ede
Slava <slavafid@gmail.com>
2017-12-05 08:39:45 +0200

    Added Fullscreen draw square and gameframe functions

--- init.py
+++ init.py
@@ -2,22 +2,41 @@ import pygame
 
 pygame.init()
 width, height = pygame.display.list_modes()[0]
-print(width,height)
-gameDisplay = pygame.display.set_mode()
+
+colors = {"white":(255,255,255),"black":(0,0,0)}
+
+
+#width -= 20
+#height -= 100
+#gameDisplay = pygame.display.set_mode((width, height))
+gameDisplay = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption('Scorched Tanks')
 clock = pygame.time.Clock()
-pygame.display.toggle_fullscreen()
 
-while True:
+def drawSquare(color, x, y, w, h):
+    pygame.draw.rect(gameDisplay, color, [x, y, w, h])
+
+def gameFrame():
+    frameX = width*0.05
+    frameY = height*0.05
+    frameW = width*0.8
+    frameH = height*0.8
+    drawSquare(colors["white"],frameX,frameY,frameW,frameH)
+    
+game = True
+while game:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+            game = False
                 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                  pygame.quit()
-                  quit()  
+                  game = False
+                  
+    gameDisplay.fill(colors["black"])
+    gameFrame()
+    pygame.display.update()
+    
     clock.tick(60)
 
+pygame.quit()
