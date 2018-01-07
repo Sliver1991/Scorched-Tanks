cf443f92b16275e1eec2831f2e455a14f0467e40
Slava Fidel <slavafid@gmail.com>
2018-01-07 08:38:24 +0200

    Added image draw-transform function

--- draw.py
+++ draw.py
@@ -23,3 +23,9 @@ def drawLeftArrow(color, x,y):
 def drawRightArrow(color, x,y):
     poly = [(x,y),(x+10,y-15),(x,y-30),(x+10,y-30),(x+20,y-15),(x+10,y)]
     drawPoly(color,poly)
+    
+def drawImage(image,x=0,y=0, transform=False, w=0,h=0):
+    if w==0: w=init.screen[0]
+    if h==0: h=init.screen[1]
+    if transform: image = pygame.transform.scale(image,(w,h))
+    init.surface.blit(image, (x,y))
